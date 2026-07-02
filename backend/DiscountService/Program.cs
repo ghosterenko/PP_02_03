33292fc37cc52291c9070adf6f9a7d5842af0df0
ghosterenko <arsenijkonkov243@gmail.com>
2026-07-02 12:55:49 +0500

    Update Program.cs

--- backend/DiscountService/Program.cs
+++ backend/DiscountService/Program.cs
@@ -6,12 +6,12 @@ app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod());
 app.MapGet("/api/discount", (string promo) =>
 {
     int percent = 0;
-
+    // Проверяем промокод и назначаем соответствующую скидку
     if (promo == "SGD10")
         percent = 10;
     if(promo == "SGD20")
         percent = 20;
-
+    // Возвращаем размер скидки в процентах
     return new { discountPercent = percent };
 
 });
