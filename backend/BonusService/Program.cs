e8a32f9332785224868b6f74bd24a65df85d16b3
ghosterenko <arsenijkonkov243@gmail.com>
2026-07-02 12:54:05 +0500

    Update Program.cs

--- backend/BonusService/Program.cs
+++ backend/BonusService/Program.cs
@@ -4,8 +4,11 @@ var app = builder.Build();
 app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod());
 
 app.MapGet("/api/bonus", (double amount) => {
+    
+    // Начисляем 20 бонусных баллов за каждые 10000 рублей
     int points = (int)(amount / 10000 * 20);
+    // Возвращаем количество бонусных баллов
     return new { bonusPoints = points };
 });
-
+// Порт 5004
 app.Run("http://localhost:5004");
