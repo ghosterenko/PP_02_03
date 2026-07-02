72550b1907ebe5bab68a2cfc74022c51a2ef761b
ghosterenko <arsenijkonkov243@gmail.com>
2026-07-02 12:58:22 +0500

    Update Program.cs

--- backend/PriceService/Program.cs
+++ backend/PriceService/Program.cs
@@ -5,10 +5,11 @@ app.UseCors(b => b.AllowAnyOrigin().AllowAnyMethod());
 app.MapGet("/api/price", (double area, string type) =>
 {
     int price = 0;
+    // Цена за квадратный метр: для бани 18000, для дома 24000
     if (type == "Баня")
         price = 18000;
     else price = 24000;
-
+    // Расчёт базовой стоимости = площадь * цена за метр - ответ в формате Json
     return new { basePrice = area * price };
 });
 
